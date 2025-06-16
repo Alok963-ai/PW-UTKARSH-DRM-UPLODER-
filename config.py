c34654256038bbe731323106f42b67f61eea7bb1
Alok963-ai <alokjangde6@gmail.com>
2025-06-16 06:54:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7")
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 7833842279))
-STRING = int(os.environ.get("STRING", AQFwCeIAOz1FS7JecfYU8zMZdNCoey8c3cbpOlG0CmPPY9mXBXyG2C0_Uf83cWS_dI38I16qpCyuggIpwc2LrcYQtUXEbyjxtfRWl3jif61NDbq95dMqvSLJkYz6xGaPas5qCfMubSwdkxgaFc_ejU1A5Fglp4RsPKnH_G4OjF_wEUAzNiWn0PhTVlUT-au6thdjfR-hsPvGUnksl4bTRb4BfR0cFsh0X8Tg14_KGJRMBGUN2URG1PO1ATGpiFDPmUDFco2SwQIW6VadpUUROTUD70z0SnAvQ7aZMD7MvpovTdHO8YJVu0GiMT5qJZKtNGHUzKj4rdxiKdHoA0_9z5h2pIR20QAAAAGrb7QLAA))
+STRING = int(os.environ.get("STRING", BQE1lbcASKtHYxpJya2lyRMtXJtJ4_jgvbufglgk8FkxoDQzc3W9mOSz54iurDHcaskuZSCoDKy19iUhZIdoILpWjUx_00VN3XuX5ux8DjUNNbH6xUojOmtN2pu5XWEmogvgbFsbZtDpyabtpCL2sxF6ivFJN7nxvk-ryOJ-eTsDBnL1hC6smI31Y1oU7uUbYhlBVewscywqC0bQOkLu82pCOVrhAYz-UK-h2ltF5zlxlAM0erlkAEZk3DdvBgqOz_9scqwfrB_tF5qZrP9Jcm8pNiEbxPtZbec3S72MHncqOCYUpsd9tlkrkrF0eLCfu5d1pvmyTq5Wx98hRg4HnRGo1cvVwgAAAAHS7vJnAA))
 
 LOG = -1002548493129
 
